da1e011defc56fbe45c90ac23ce3a1bbe823db6e
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-11-23 17:34:57 +0900

    애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,7 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -31,12 +33,19 @@ public class NetworkClient { // implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
+    // @PostConstruct, @PreDestroy 특징
+    // 애노테이션 하나만 붙이면 되기 때문에 편리함
+    // 스프링이 아닌 다른 컨테이너에서도 동작함
+    // 컴포넌트 스캔과도 잘 어울림
+    // 유일한 단점: 외부 라이브러리에는 적용하지 못함
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
